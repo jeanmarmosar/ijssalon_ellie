6a6092a900b6e4bc8bd8456eb074f201c2cbed64
jean-marc mosar <cald@jean-marc.be>
2024-02-27 00:43:56 +0100

    functies toegevoegd

--- helper.py
+++ helper.py
@@ -5,3 +5,12 @@ print(lengte * "*")
 print(f"* {tekst} *")
 print(lengte * "*")
 print()
+
+
+def decoreer(tekst=""):
+    lengte = len(tekst) + 4
+    print()
+    print(lengte * "*")
+    print(f"* {tekst} *")
+    print(lengte * "*")
+    print()
