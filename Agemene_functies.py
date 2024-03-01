4a6504105154ca947734f30283ab735803048c6d
jean-marc mosar <cald@jean-marc.be>
2024-03-01 16:09:41 +0100

    update les 8

--- Agemene_functies.py
+++
@@ -1,6 +0,0 @@
-number = 7
-square=number + number
-print(square)
-
-        
-    
---
+++ Algemene_functies.py
@@ -0,0 +1,21 @@
+
+
+def mijn_functie_1(mijn_int): 
+    return mijn_int ** 2
+
+print(mijn_functie_1(4))
+    
+
+
+
+
+
+
+
+
+
+
+
+
+   
+    
