b4b863f1804995f8711c62206a720ecd80b9412a
jean-marc mosar <cald@jean-marc.be>
2024-03-05 12:30:11 +0100

    update les 8 huiswerk

--- reclame.py
+++ reclame.py
@@ -1,10 +1,10 @@
 
 def Aanbieding_1():  
-   #na de goede output komt ook 'None' te staan
+   #na de goede output komt ook 'None' te staan?
    #3.60 met :.2f gelukt
     mijn_diction = {
         "smaak" : "aardbei",  
-        "prijs" : 4.00,  
+        "prijs" : 4,  
         "korting" : 0.1
         }
     reclamesmaak=(mijn_diction["smaak"])
@@ -17,3 +17,26 @@ def Aanbieding_1():
     print(reclame_tekst)
     
 print(Aanbieding_1())
+
+
+def inkomsten_totaal(inkomsten):
+    #na de goede output komt ook 'None' te staan?
+    inkomsten=sum([220, 430, 125, 160, 205, 90, 345])
+
+    print(inkomsten)
+#om het totaal te bekomen heb ik 2 "" geplaatst om niet terug de inkomsten te moeten  ingeven
+#dus print(inkomsten_totaal(inkomsten)) werkt niet 
+print(inkomsten_totaal(""))
+
+
+
+ 
+
+    
+
+ 
+    
+    
+
+
+    
